dfa80db596d3e93cb852387c55ca658ba10c32ca
Saiguna7 <110450241+Saiguna7@users.noreply.github.com>
2024-03-21 19:54:50 +0530

    changes are made

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import AboutSection from "@/components/AboutSection";
 import AchievementsSection from "@/components/AchievementsSection";
 import Emailsection from "@/components/Emailsection";
@@ -6,23 +6,23 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
 import ProjectsSection from "@/components/ProjectsSection";
-import Splin3d from '@/components/Splin3d';
-import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+// import Splin3d from '@/components/Splin3d';
+// import { usePathname } from 'next/navigation';
+// import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const pathname=usePathname();
-const isHome= pathname==='/'
-const [isloading,setloading]=useState(isHome);
-useEffect(()=>{
-  if(isloading) return
-},[isloading])
+//   const pathname=usePathname();
+// const isHome= pathname==='/'
+// const [isloading,setloading]=useState(isHome);
+// useEffect(()=>{
+//   if(isloading) return
+// },[isloading])
   return (
     <>
-    {
+    {/* {
       isloading && isHome ?(<Splin3d finishloading={()=>setloading(false)}/>):
 
-      (
+      ( */}
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
@@ -34,8 +34,8 @@ useEffect(()=>{
       </div>
       <Footer />
     </main>
-    )
-}
+    {/* )
+} */}
     </>
   );
 }
