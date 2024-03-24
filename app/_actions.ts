b6d014b5fcd164df9acfc2753c00c25ff8584ba0
Saiguna7 <110450241+Saiguna7@users.noreply.github.com>
2024-03-24 17:53:50 +0530

    changes

--- app/_actions.ts
+++ app/_actions.ts
@@ -1,34 +1,37 @@
 "use server"
-import {z} from "zod"
+import { z } from "zod"
 import { Resend } from "resend"
 import { ContactFormSchema } from "@/schemas/ContactForm"
 import ContactFormEmail from "@/emails/contant-email-form"
-type ContactFormInputs=z.infer<typeof  ContactFormSchema>
-const resend=new Resend(process.env.RESEND_API_KEY)
-export async function sendEmail(data: ContactFormInputs) {
-    const result=ContactFormSchema.safeParse(data)
-    if(result.success){
-        const {name,email,subject,message}=result.data
-        try{
-            const data=await resend.emails.send({
-                from:'kloudcodes <email@kloudcodes.com>',
-                to:'info.kloudcode@gmail.com',
-                subject:subject,
-                reply_to: email,
-                text:`Name:${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
-                react: ContactFormEmail({name,email,subject,message})
-            })
-            return {success:true,data}
-            
-        }catch(error:Error | any){
-            console.error("Error digest:", error.digest);
-            return {success: false,error}
-            
-        }
-    }
-        if(result.error){
-            return {success:false, error:result.error.format()}
-            
-        }
-    }
 
+type ContactFormInputs = z.infer<typeof ContactFormSchema>
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function sendEmail(data: ContactFormInputs) {
+  const result = ContactFormSchema.safeParse(data)
+
+  if (result.success) {
+    const { name, email, subject, message } = result.data
+
+    try {
+      const emailData = await resend.emails.send({
+        from: 'kloudcodes <email@kloudcodes.com>',
+        to: 'info.kloudcode@gmail.com',
+        subject: subject,
+        reply_to: email,
+        text: `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`,
+        react: ContactFormEmail({ name, email, subject, message })
+      })
+
+      return { success: true, data: emailData }
+    } catch (error: any) {
+      console.error("Error sending email:", error);
+      return { success: false, error: error.toString() }
+    }
+  }
+
+  if (result.error) {
+    const errorMessage = result.error.format().toString()
+    return { success: false, error: errorMessage }
+  }
+}
